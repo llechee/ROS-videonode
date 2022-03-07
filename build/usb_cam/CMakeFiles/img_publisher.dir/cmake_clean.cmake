748eeabfe76c43d19d3ce97b99deeea2a38752bb
leechee <2543012055@qq.com>
2022-03-07 02:24:20 -0800

    first

---
+++ build/usb_cam/CMakeFiles/img_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lz/video_ws/devel/lib/usb_cam/img_publisher"
+  "/home/lz/video_ws/devel/lib/usb_cam/img_publisher.pdb"
+  "CMakeFiles/img_publisher.dir/src/img_publisher.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/img_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
